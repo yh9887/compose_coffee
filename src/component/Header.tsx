bd2412360a3ebcd740c96f6787653ef049fa4fc8
함우성 <wooseongham@gmail.com>
2023-04-10 20:14:24 +0900

    헤더 오류 해결

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import FA from 'react-fontawesome';
-import { Link } from 'react-router-dom';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import FA from "react-fontawesome";
 
-function Header(){
-  return(
+function Header() {
+  return (
     <div className="header">
       <div className="header_wrap">
-      <Link to='/'><img src="./img/logo-light.svg" alt="" className="logo" /></Link>
+        <Link href="/">
+          <Image src="./img/logo-light.svg" alt="" className="logo" />
+        </Link>
         <div className="header_menu">
           <ul className="menu_ul">
-            <Link to='/brand'><li className="menu_li">BRAND</li></Link>
-            <Link to='/#'><li className="menu_li">FRANCHISE</li></Link>
-            <Link to='/menu'><li className="menu_li">MENU</li></Link>
-            <Link to='/#'><li className="menu_li">STORE</li></Link>
-            <Link to='/news'><li className="menu_li">NEWS</li></Link>
+            <Link href="/brand">
+              <li className="menu_li">BRAND</li>
+            </Link>
+            <Link href="/#">
+              <li className="menu_li">FRANCHISE</li>
+            </Link>
+            <Link href="/menu">
+              <li className="menu_li">MENU</li>
+            </Link>
+            <Link href="/#">
+              <li className="menu_li">STORE</li>
+            </Link>
+            <Link href="/news">
+              <li className="menu_li">NEWS</li>
+            </Link>
             <div className="menu_icon">
               <FA name="instagram" />
               <FA name="apple" />
@@ -22,12 +35,13 @@ function Header(){
           </ul>
         </div>
         <div className="ham_menu">
-          <a href="" className="icon"><i className="fa fa-thin fa-bars"></i></a>
+          <a href="" className="icon">
+            <i className="fa fa-thin fa-bars"></i>
+          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Header;
-
