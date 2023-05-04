84c732991f5915daf5f536a94cf5be29e391f1c3
함우성 <wooseongham@gmail.com>
2023-05-04 20:40:16 +0900

    notice API 생성

---
+++ pages/api/notices.ts
@@ -0,0 +1,21 @@
+import prisma from "@lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const result = await prisma.notice.findMany({
+      select: {
+        id: true,
+        imageKey: true,
+        title: true,
+        body: true,
+        viewCount: true,
+        createdAt: true,
+      },
+    });
+    return res.status(200).json(result);
+  }
+}
