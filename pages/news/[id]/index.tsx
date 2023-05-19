71a5168bd207b9c73cfcb6252f1963859739512d
Younghee Hong <gmlakddl987@naver.com>
2023-05-19 18:07:29 +0900

    공지사항 작업

--- pages/news/[id]/index.tsx
+++ pages/news/[id]/index.tsx
@@ -1,7 +1,12 @@
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { faHouseChimney } from "@fortawesome/free-solid-svg-icons";
+import DownMenu from "@components/Down_menu";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 function NewsShow({ id }) {
   const [notice, setNotice] = useState({
@@ -28,8 +33,50 @@ function NewsShow({ id }) {
   return (
     <>
       <Header />
-      <div>{notice.title}</div>
+      <div className="sec1">
+          <img src="../img/news/banner.jpg" alt="" />
+          <Link href={"/news/1"}>
+            <h1 className="sec1_title">NEWS</h1>
+          </Link>
+          <div className="nav">
+            <div className="home">
+              <FontAwesomeIcon
+                icon={faHouseChimney}
+                className={"fa-solid fa-house-chimney "}
+              />
+            </div>
+            <div className="nav_menu">
+              <div className="top_menu">
+                <span>NEWS</span>
+                <FontAwesomeIcon
+                  icon={faAngleDown}
+                  className={"fa-sharp fa-solid fa-angle-down"}
+                />
+              </div>
+              <DownMenu />
+            </div>
+            <div className="about">
+              <div className="top_menu">
+                <span>공지사항</span>
+                <FontAwesomeIcon
+                  icon={faAngleDown}
+                  className={"fa-sharp fa-solid fa-angle-down"}
+                />
+              </div>
+              <DownMenu pageName="news" />
+            </div>
+          </div>
+        </div>
+      <div className="notice_wrap">
+      <h1 className="notice_title">{notice.title}</h1>
+      <div className="title_info">
+        <span className="notice_createdAt">{notice.createdAt}</span>
+        <span>{notice.viewCount}</span>
+      </div>
+      <div className="notice_border"></div>
       <div>{notice.body}</div>
+
+      </div>
       <Footer />
     </>
   );
